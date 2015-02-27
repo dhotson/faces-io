9d6543701bcfba57182f3f3ce2f7a63c0432afb7
Dennis Hotson <dennis@99designs.com>
2015-02-28 00:15:40 +1100

    Fixed bugs

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-var express = require('express')
-var app = express()
 var twilio = require('twilio')(process.env.TWILIO_ACCOUNTSID, process.env.TWILIO_AUTHTOKEN);
 
 var ICE_SERVERS = [{
     "url": "stun:stun.l.google.com:19302"
 }];
 var updateIceServers = function() {
-    client.tokens.post({}, function(err, token) {
+    twilio.tokens.post({}, function(err, token) {
         if (!err) {
             ICE_SERVERS = token.iceServers;
         } else {
@@ -15,17 +13,20 @@ var updateIceServers = function() {
                 "url": "stun:stun.l.google.com:19302"
             }];
         }
-    })
+    });
 };
-setInterval(updateIceServers, 5000);
+updateIceServers();
+setInterval(updateIceServers, 3600*1000*12); // Update every 12 hours
 
+var express = require('express')
+var app = express()
 app.get('/ice.js', function (req, res) {
     res.set('Content-Type', 'text/javascript');
     res.send('var ICE_SERVERS='+JSON.stringify(ICE_SERVERS));
 });
 
 var port = parseInt(process.env.PORT, 10) || 8001;
-var server = app.listen(3000);
+var server = app.listen(port);
 
 var webRTC = require('webrtc.io').listen(server);
 
